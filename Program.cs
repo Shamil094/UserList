4e6a04d2d3801838bb42309079befc3aa428ae30
Shamil Celilzade <shamillcelilzade@gmail.com>
2022-12-16 22:05:31 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> user = new List<string>();
+            user.Add("Shamil Celilzade");
+            user.Add("Ibrahim Huseynov");
+            user.Add("Elcan Musali");
+            user.Add("Cavid Bunyatov");
+
+            UserList ul = new UserList();
+            ul.GetUser(user);
+
+
+            Console.WriteLine(ul.Name);
+            Console.WriteLine(ul.Surname);
+
+            Console.Read();
+        }
+    }
+}
---
+++ UserList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserList
+{
+    internal class UserList
+    {
+        public string Name;
+        public string Surname;
+
+        public void GetUser(List<string> users)
+        {
+            foreach (string item in users)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (string item in users)  //ozumden yoxlamaq ucun yazmisham
+            {
+                Name = item.Substring(0, item.IndexOf(' '));
+                Surname = item.Substring(item.IndexOf(' ') + 1);
+            }
+        }
+    }
+}
